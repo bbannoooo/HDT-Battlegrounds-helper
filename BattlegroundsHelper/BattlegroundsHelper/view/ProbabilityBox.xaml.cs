3be119b198758b82f5876a70cc0a8474df0ed885
b4no <bbannoooo@gmail.com>
2020-12-17 00:50:48 +0900

    Update overlay + test for text changes

--- BattlegroundsHelper/BattlegroundsHelper/view/ProbabilityBox.xaml.cs
+++ BattlegroundsHelper/BattlegroundsHelper/view/ProbabilityBox.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Hearthstone_Deck_Tracker.API;
+﻿using Hearthstone_Deck_Tracker;
 using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.Windows;
 using System;
+using Hearthstone_Deck_Tracker.Utility;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +16,46 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Hearthstone_Deck_Tracker.API;
 
 namespace BattlegroundsHelper.view
 {
     /// <summary>
     /// probabilityBox.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class probabilityBox : UserControl
+    public partial class ProbabilityBox : UserControl
     {
-        public probabilityBox()
+
+
+        public ProbabilityBox()
         {
             InitializeComponent();
+
+            tier1.Probability = new Probability()
+            {
+                tier = "Tier1"
+            };
+            tier2.Probability = new Probability()
+            {
+                tier = "Tier2"
+            };
+            tier3.Probability = new Probability()
+            {
+                tier = "Tier3"
+            };
+            tier4.Probability = new Probability()
+            {
+                tier = "Tier4"
+            };
+            tier5.Probability = new Probability()
+            {
+                tier = "Tier5"
+            };
+            tier6.Probability = new Probability()
+            {
+                tier = "Tier6"
+            };
+            Calculate(1);
         }
 
         internal void SetProbability()
@@ -51,5 +82,82 @@ namespace BattlegroundsHelper.view
         {
             this.Visibility = Visibility.Hidden;
         }
+
+        private void probabilityBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            tierbox.Visibility = Visibility.Visible;
+        }
+
+        private void probabilityBox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            tierbox.Visibility = Visibility.Hidden;
+        }
+
+        internal void Calculate(int heropower)         // 아직 계산 미구현 임시로 확률 나타냄
+        {
+            switch(heropower) 
+            {
+                case 1:
+                    tier1.percentageBox.Text = "100%";
+                    tier2.percentageBox.Text = "0%";
+                    tier3.percentageBox.Text = "0%";
+                    tier4.percentageBox.Text = "0%";
+                    tier5.percentageBox.Text = "0%";
+                    tier6.percentageBox.Text = "0%";
+                    break;
+
+                case 2:
+                    tier1.percentageBox.Text = "80%";
+                    tier2.percentageBox.Text = "20%";
+                    tier3.percentageBox.Text = "0%";
+                    tier4.percentageBox.Text = "0%";
+                    tier5.percentageBox.Text = "0%";
+                    tier6.percentageBox.Text = "0%";
+                    break;
+                case 3:
+                    tier1.percentageBox.Text = "60%";
+                    tier2.percentageBox.Text = "30%";
+                    tier3.percentageBox.Text = "10%";
+                    tier4.percentageBox.Text = "0%";
+                    tier5.percentageBox.Text = "0%";
+                    tier6.percentageBox.Text = "0%";
+                    break;
+                case 4:
+                    tier1.percentageBox.Text = "20%";
+                    tier2.percentageBox.Text = "30%";
+                    tier3.percentageBox.Text = "30%";
+                    tier4.percentageBox.Text = "20%";
+                    tier5.percentageBox.Text = "0%";
+                    tier6.percentageBox.Text = "0%";
+                    break;
+                case 5:
+                    tier1.percentageBox.Text = "10%";
+                    tier2.percentageBox.Text = "20%";
+                    tier3.percentageBox.Text = "30%";
+                    tier4.percentageBox.Text = "30%";
+                    tier5.percentageBox.Text = "10%";
+                    tier6.percentageBox.Text = "0%";
+                    break;
+                case 6:
+                    tier1.percentageBox.Text = "10%";
+                    tier2.percentageBox.Text = "10%";
+                    tier3.percentageBox.Text = "30%";
+                    tier4.percentageBox.Text = "30%";
+                    tier5.percentageBox.Text = "20%";
+                    tier6.percentageBox.Text = "10%";
+                    break;
+                default:
+                    tier1.percentageBox.Text = "0%";
+                    tier2.percentageBox.Text = "0%";
+                    tier3.percentageBox.Text = "0%";
+                    tier4.percentageBox.Text = "0%";
+                    tier5.percentageBox.Text = "0%";
+                    tier6.percentageBox.Text = "0%";
+                    break;
+            }
+
+        }
+
+
     }
 }
---
+++ BattlegroundsHelper/BattlegroundsHelper/view/ProbabilityControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Hearthstone_Deck_Tracker.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BattlegroundsHelper.view
+{
+    /// <summary>
+    /// ProbabilityControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ProbabilityControl : UserControl
+    {
+        private Probability _probability;
+        public Probability Probability
+        {
+            get
+            {
+                return _probability;
+            }
+
+            set
+            {
+                _probability = value;
+                tierBox.Text = _probability.tier;
+                percentageBox.Text = _probability.percentage;
+            }
+        }
+
+        public ProbabilityControl()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Probability
+    {
+        public string tier { get; set; }
+        public string percentage { get; set; }
+    }
+
+
+
+}
